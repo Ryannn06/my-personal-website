7034392d30e7b2f1351c02297912f653d9987a39
Ryannn06 <ryanangelo.delacruz12@gmail.com>
2025-11-21 00:00:08 +0800

    add project

--- src/app/_components/Home/MyProject.jsx
+++ src/app/_components/Home/MyProject.jsx
@@ -11,36 +11,37 @@ import { useEffect, useState } from 'react';
 
 const projectArray = [
 	{
+		title: "Analysis of Grade 6 NAT Results",
+		description: "A Python analysis on the DepEd NAT MPS Results, SY 2023-2024",
+		github_link: "https://github.com/Ryannn06/Analysis-of-Grade-6-National-Achievement-Test-MPS-2023-2024",
+		date_:"2025.11.20",
+		badge: ["Python", "Pandas", "Seaborn"]
+	},
+	{
 		title: "Analysis of Deped Masterlist",
 		description: "A SQL analysis and Power BI dashboard on the DepEd Schools Masterlist 64k+ dataset",
 		github_link: "https://github.com/Ryannn06/SQL-Case-Study-on-DepEd-Schools-Masterlist",
-		bg_color: "#add0b3",
 		date_:"2025.11.16",
-		image:"1.png",
 		badge: ["Python", "PostgreSQL", "Power BI"]
 	},
 	{
 		title: "Uber Customer Reviews 2024",
 		description: "A Tableau dashboard, visualizing key insights from 12,000+ Uber reviews in Google Play Store",
 		github_link: "https://public.tableau.com/views/UberCustomerReviews/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-		bg_color: "#add0b3",
 		date_:"2025.02.07",
-		image:"1.png",
 		badge: ["Tableau"]
 	},
 	{
 		title: "Mobile App Activity Analysis",
 		description: "An analysis using Python, uncovering user behaviour patterns, activity trends, and predictions",
 		github_link: "https://www.kaggle.com/code/ryanangelodelacruz/mobile-app-screen-time-eda-mla",
-		bg_color: "#ffd1dc",
 		date_:"2025.01.06",
-		badge: ["Python", "Pandas", "Matplotlib"]
+		badge: ["Python", "Pandas", "Seaborn", "Scikit-learn"]
 	},
 	{
 		title: "Spotify Streams 2024",
 		description: "A Tableau dashboard, exploring Spotify's 2024 most streamed tracks and albums",
 		github_link: "https://public.tableau.com/views/Spotify_Streams_17353873277910/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-		bg_color: "#ffd1dc",
 		date_:"2024.12.28",
 		badge: ["Tableau"]
 	},
@@ -48,8 +49,6 @@ const projectArray = [
 		title: "Thesis Archive system",
 		description: "A Digital Thesis Archive for storing of e-thesis documents built in Django and MySQL",
 		github_link: "https://github.com/Ryannn06/DIGITAL-THESIS-ARCHIVE-DJANGO-PROJECT",
-		bg_color: "#ff6961",
-		image:"Thesis_archive2.svg",
 		date_:"2023.01.16",
 		badge: ["Django", "MySQL", "Bootstrap"]
 	},
